7b02bc4ab3d8fb011ec2e0fc87275ade9b18db06
Natasha <natashakhema@gmail.com>
2018-12-23 22:05:05 +1100

    add time component

--- src/index.js
+++ src/index.js
@@ -2,25 +2,42 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 //react component
-const App = () => {
-    const buttonText = "Click Me!";
+// const App = () => {
+//     const buttonText = "Click Me!";
 
-    return(
+//     return(
+//         <div>
+//             <label className = "label" htmlFor="firstname">
+//             Enter first name
+//             </label>
+//             <input type="text" name="firstname" />
+//             <label className= "label" htmlFor="lastname">
+//             Enter last name
+//             </label>
+//             <input type="text" name="lastname" />
+//             <button style={{ backgroundColor: "blue", color: "white" }}>
+//             { buttonText}
+//             </button>  
+//         </div>
+//     );
+// };
+
+//Get current time
+function getTime() {
+    return(new Date()).toLocaleTimeString()
+
+}
+
+
+const App = () => {
+    return (
         <div>
-            <label className = "label" for="firstname">
-            Enter first name
-            </label>
-            <input type="text" name="firstname" />
-            <label className= "label" for="lastname">
-            Enter last name
-            </label>
-            <input type="text" name="lastname" />
-            <button style={{ backgroundColor: "blue", color: "white" }}>
-            { buttonText}
-            </button>  
+            <h1>Current Time:</h1>
+            <h3>{ getTime() }</h3>
         </div>
     );
-};
+}
+
 
 ReactDOM.render(
   <App />,
